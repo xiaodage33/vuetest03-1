8f1ad715b7ae32fea86836c570ed04b37f02db76
“admin” <542035984@qq.com>
2020-05-20 13:13:59 +0800

    sessionStorage.setItem('isCollapse',JSON.stringify(state.isCollapse))进行保存和取值在 浏览器Application中存取

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isCollapse: false,
-      count: 10
+    // isCollapse: false,
+    //   count: 10
+      //优先到浏览器取值，没有则是false
+    // isCollapse: JSON.parse(sessionStorage.getItem('isCollapse')) || false
+//取本地存储
+      isCollapse: localStorage.getItem('isCollapse') || false
+
+
   },
   getters:{   //computed
     count: state => state.count +10
@@ -18,17 +24,26 @@ export default new Vuex.Store({
 
   mutations: {  //修改上面count在这里修改
 
+    // SET_COLLAPSE(state){
+    //   // console.log(333333)
+    //   state.isCollapse = !state.isCollapse
+    //    // console.log(state.isCollapse)
+    // },
     SET_COLLAPSE(state){
-      // console.log(333333)
-      state.isCollapse = !state.isCollapse
-       // console.log(state.isCollapse)
-    },
+          state.isCollapse = !state.isCollapse
+          // html5本地存储
+          sessionStorage.setItem('isCollapse',JSON.stringify(state.isCollapse))
+        //存储到本地除非删除本地cookie
+        //   localStorage.setItem('isCollapse',JSON.stringify(state.isCollapse))
+        }
 
 
-    SET_COUNT(state,value){
-      state.count = value
-      console.log(state.count)
-    }
+    // SET_COUNT(state,value){
+    //   state.count = value
+    //   console.log(state.count)
+    // },
+
+
 
     },
   actions: {
