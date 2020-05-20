2efcd6c63f22957b6f650ad9cd3cb95ee56990c5
“admin” <542035984@qq.com>
2020-05-20 21:21:21 +0800

    后台样式 重新调整后 ok --同名模块区分命名空间app和login ，里面方法可以同名

--- src/main.js
+++ src/main.js
@@ -14,13 +14,21 @@ import 'element-ui/lib/theme-chalk/index.css';
 /**自定义全局组件名称**/
 import "./icons";
 
-
-
 Vue.use(ElementUI);
 Vue.use(VueCompositionApi);
 
-
 Vue.config.productionTip = false;
+
+//路由守卫
+router.beforEach((to,from,next)=>{
+  console.log(to)
+      console.log(from)
+  console.log(next)
+
+
+})
+
+
 
 new Vue({
   router,
