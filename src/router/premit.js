022ce3cf0ddfeec88fcf143a1f3fc44e4d1d81a1
“admin” <542035984@qq.com>
2020-05-21 12:15:26 +0800

    检测token是否存在  返回到login页面

--- src/router/premit.js
+++ src/router/premit.js
@@ -1,12 +1,32 @@
-//路由守卫
 import router from "./index";
+import { getToken } from "../utils/app";
 
+
+//声明白名单
+const whiteRouter = ['/login']; //indexOf方法，判断数字中是否存在指定的某个对象,如果不存在返回-1
+
+//路由守卫
 router.beforeEach((to, from, next)=>{
+  if(getToken()){
+      //路由动态添加，分配菜单，每个角色分配不同的菜单
+      console.log('存在token')
+  }else{
+    //不能直接使用  next('/login') 不然会一直跳转
+/**直接进入index，参数to被改变成/index 出发路由指向，就会跑beforeEach；
+ * 再一次next 指向了 login ，再次发送路由指向，再跑beforeEach ，参数的to被改变成/login
+ * 白名单判断存在，则直接执行next（），因为没有参数，所以不会再次beforeEach**/
+    console.log('不存在')
+    console.log(to.path)
+    if(whiteRouter.indexOf(to.path) !== -1) {
+      next()
+    }
+    else{
+          next('/login')
+        }
+}
   console.log(to)  //进入页面（下一个页面）
   console.log(from)  //离开前的页面上一个
   console.log(next)
   next()
 //token令牌
-
-
 })
--- src/utils/app.js
+++ src/utils/app.js
@@ -1,2 +1,7 @@
 import cookie  from "cookie_js";
 //引入cookie后在 路由 拦截那 判断 cookie 是否有token 在进行进入
+
+export function getToken(){
+    return cookie.get('admin_token')
+
+}
